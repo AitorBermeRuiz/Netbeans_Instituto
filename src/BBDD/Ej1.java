4b7b613d426c442c789edd0a5a2611bfaaf207b3
aitor0405 <aitorbermeruiz@gmail.com>
2022-01-25 22:45:42 +0100

    v1BBDD

---
+++ src/BBDD/Ej1.java
@@ -0,0 +1,39 @@
+/*
+Una vez creada la base de datos hacer un programa en Java que cree una nueva
+ tabla llamada NotasFinales que tendrá la siguiente estructura:
+ NotasFinales(Mat,Cod,NotaMedia);
+ Y cuyos valores se sacaran de la tabla Notas.
+ Por último se imprimirá un listado de todos los alumnos con el siguiente formato:
+
+ Nombre Alumno Nombre Asignatura Nota1 Nota2 Nota3 NotaMedia
+ ------------- -----------------  -      -     -      -
+ ------------- -----------------  -      -     -      -
+ ------------- -----------------  -      -     -      -
+ */
+package BBDD;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.*;
+
+public class Ej1 {
+    public static void main(String[] args) {
+        try {
+            
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn =DriverManager.getConnection("jdbc:mysql://localhost/instituto","root","");
+            Statement st=conn.createStatement();
+            
+            
+            
+            
+            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Ej1.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Ej1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+}
